2c7e1a79ca6abd9e0256207a347f712554ab026b
mohi <mohisadeghi302@gmail.com>
2023-02-10 12:59:23 +0330

    Add styles

--- src/modules/images.js
+++ src/modules/images.js
@@ -1,5 +1,4 @@
 import mealLogo from '../assets/icons/meal-logo.png';
-import background from '../assets/image/image3.jpg';
 const mealLogo1 = new Image();
 mealLogo1.src = mealLogo;
 document.querySelector('.meal-logo-div').append(mealLogo1);
